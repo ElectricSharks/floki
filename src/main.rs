e14bbedd22f60885fe615e2c8c900163560c6a45
Electric Shark <electricshark@example.com>
2022-02-04 16:25:06 +0000

    Modified main.rs to append from HOME/.floki/startup.sh

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,10 @@ use config::FlokiConfig;
 use environment::Environment;
 use structopt::StructOpt;
 
+use std::path::Path;
+use std::fs;
+use std::env;
+
 fn main() -> Result<(), Error> {
     let args = Cli::from_args();
     configure_logging(args.verbosity)?;
@@ -41,6 +45,10 @@ fn run_floki_from_args(args: &Cli) -> Result<(), Error> {
         warn!("-l/--local is deprecated and may be removed in a future release");
     }
 
+
+    // Check whether a floki config file exists, if so, load the extra command
+    // and append to the inner command.
+
     // Dispatch appropriate subcommand
     match &args.subcommand {
         // Pull the image in the configuration file
@@ -55,6 +63,7 @@ fn run_floki_from_args(args: &Cli) -> Result<(), Error> {
             let env = Environment::gather(&args.config_file)?;
             let config = FlokiConfig::from_file(&env.config_file)?;
             let inner_command = interpret::command_in_shell(config.shell.inner_shell(), command);
+            let inner_command = append_global_config(inner_command.as_str());
             interpret::run_floki_container(&spec::FlokiSpec::from(config, env)?, &inner_command)
         }
 
@@ -67,10 +76,34 @@ fn run_floki_from_args(args: &Cli) -> Result<(), Error> {
         None => {
             let env = Environment::gather(&args.config_file)?;
             let config = FlokiConfig::from_file(&env.config_file)?;
-            let inner_command = config.shell.inner_shell().to_string();
+            let inner_command = config.shell.inner_shell();
+            let inner_command = append_global_config(inner_command);
             interpret::run_floki_container(&spec::FlokiSpec::from(config, env)?, &inner_command)
         }
     }
+}
+
+/// Searches for a startup script in $HOME/.floki, if found, will run commands
+/// in the floki container when the container starts up.
+fn append_global_config(command: &str) -> String {
+    // Retrieve the name of the home directory.
+    let home = match env::var("HOME") {
+        Ok(val) => val,
+        Err(_) => return command.to_string(),
+    };
+
+    // Check for the existence of the startup script.
+    let filepath = format!("{}/.floki/startup.sh", home.as_str());
+    let startup_script_exists = Path::new(&filepath).exists();
+    if startup_script_exists {
+        let contents = match fs::read_to_string(filepath) {
+            Ok(data) => data,
+            Err(_) => return command.to_string(),
+        };
+        return format!("{} && {}", contents.trim(), command)
+    }
+
+    command.to_string()
 }
 
 /// Configure the logger
